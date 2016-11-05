af1743d22b86c423f196fdbf664adc965fe5d747
Dmitriy34 <webd347@ya.ru>
2016-11-05 15:49:50 +0500

    Расширяем свое приложение (сделал возможность вывода просмотра поста на отдельной странице, т.е. вывод отдельного поста на новую страницу при нажатие на заголовок)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-
+from django.shortcuts import render, get_object_or_404  ##см. "Расщиряем свое приложение"
 
 # Create your views here.
 def post_list(request):
@@ -12,3 +12,8 @@ def post_list(request):
 # который принимает в request в качестве аргумента
 # и return (возвращает) результат работы метода render,
 # который соберет наш шаблон страницы blog/post_list.html.
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
