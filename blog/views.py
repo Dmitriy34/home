c93fe3efd31fce3acb822261b43cbc8ade341a85
Dmitriy34 <webd347@ya.ru>
2016-11-05 14:28:16 +0500

    end Шаблоны Django

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # Как ты можешь заметить, мы создали метод (def) с именем post_list,
 # который принимает в request в качестве аргумента
 # и return (возвращает) результат работы метода render,
 # который соберет наш шаблон страницы blog/post_list.html.
-
